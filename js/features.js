72d5893305242f12be2d01ea630f660a25962772
sunnah117 <mohsinajahan2005@gmail.com>
2024-09-25 10:42:14 +0600

    made the total budget and cards buttons active

--- js/features.js
+++ js/features.js
@@ -1,1 +1,91 @@
-console.log('hello from features');
+document.getElementById('donate-money-noakhali').addEventListener('click',function(event){
+     event.preventDefault();
+     // const addMoney=parseFloat(document.getElementById('input-add-money-noakhali').value);
+     const donationAmount=getInputFieldValueById('input-add-money-noakhali');
+
+
+     if (!isNaN(donationAmount) && donationAmount>0) {
+          
+const balanceElement=document.getElementById('btn-add-money-noakhali');
+const currentBalance = parseFloat(balanceElement.innerText);
+const newBalance=donationAmount+currentBalance;
+balanceElement.innerText = `${newBalance} BDT`;
+
+const totalbudget=document.getElementById('total-budget');
+const currentbudget=parseFloat( totalbudget.innerText);
+const newbudget=currentbudget-donationAmount;
+totalbudget.innerText=`${newbudget} BDT`;
+console.log(newbudget)
+
+// succssful modal showing
+showSuccessModal();
+
+     }
+     else{
+          alert('failed to add the money');
+     }
+})
+
+// feni
+document.getElementById('donate-money-feni').addEventListener('click',function(event){
+     event.preventDefault();
+     // const addMoney=parseFloat(document.getElementById('input-add-money-noakhali').value);
+     const donationAmount=getInputFieldValueById('input-add-money-feni');
+     console.log(donationAmount);
+
+     if (!isNaN(donationAmount) && donationAmount>0) {
+          
+const balanceElement=document.getElementById('btn-add-money-feni');
+
+const currentBalance = parseFloat(balanceElement.innerText);
+
+const newBalance=donationAmount+currentBalance;
+
+balanceElement.innerText = `${newBalance} BDT`;
+
+const totalbudget=document.getElementById('total-budget');
+const currentbudget=parseFloat( totalbudget.innerText);
+const newbudget=currentbudget-donationAmount;
+totalbudget.innerText=`${newbudget} BDT`;
+console.log(newbudget)
+// succssful modal showing
+showSuccessModal();
+
+     }
+     else{
+          alert('failed to add the money');
+     }
+})
+
+// quota
+document.getElementById('donate-money-quota').addEventListener('click',function(event){
+     event.preventDefault();
+     // const addMoney=parseFloat(document.getElementById('input-add-money-noakhali').value);
+     const donationAmount=getInputFieldValueById('input-add-money-quota');
+
+
+     if (!isNaN(donationAmount) && donationAmount>0) {
+          
+const balanceElement=document.getElementById('btn-add-money-quota');
+const currentBalance = parseFloat(balanceElement.innerText);
+const newBalance=donationAmount+currentBalance;
+balanceElement.innerText = `${newBalance} BDT`;
+
+ const totalbudget=document.getElementById('total-budget');
+ const currentbudget=parseFloat( totalbudget.innerText);
+ const newbudget=currentbudget-donationAmount;
+ totalbudget.innerText=`${newbudget} BDT`;
+ console.log(newbudget);
+
+showSuccessModal();
+// succssful modal showing
+
+
+     }
+     else{
+          alert('failed to add the money');
+     }
+})
+
+
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,29 @@
-console.log('hello from utilities');
+function getInputFieldValueById(id){
+     const inputvalue=document.getElementById(id).value;
+     const inputNumber=parseFloat(inputvalue);
+     return inputNumber;
+}
+
+
+function getTextFieldValueById(id){
+     const textValue=document.getElementById(id).innerText;
+     const textNumber=parseFloat(textValue);
+     return textNumber;
+}
+
+
+function showModal(modalId) {
+     // Function to show a modal by removing the hidden class
+     const modal = document.getElementById(modalId);
+     modal.classList.remove('hidden');
+ }
+ 
+ function closeModal(modalId) {
+     // Function to hide a modal by adding the hidden class
+     const modal = document.getElementById(modalId);
+     modal.classList.add('hidden');
+ }
+
+ function showSuccessModal() {
+     const successModal = document.getElementById('success-modal');
+     successModal.showModal();}
